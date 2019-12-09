ddc9fc0a14d1c1e9cb43c25de01b557c0ea9c5d3
Tran Van Thuan <thuantr9802@gmail.com>
2019-12-09 14:48:29 +0700

    Create Theme

--- Magento_Theme/web/js/breadcrumbs.js
+++
@@ -1,9 +0,0 @@
-define([
-    'jquery'
-    ], function($) {
-        require(["js/breadcrumbs.js"], function(){
-            $('h1 .base').html(function(i, html){
-                return html.replace(/(\w+\s)/, '<span class="black">$1</span>')
-             })
-        });
-    });
